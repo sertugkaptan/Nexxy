544467097120dd302a3c0da4cfcab50227d9b561
sertugkaptan <sertuhan00@gmail.com>
2024-02-04 01:24:22 +0100

    Beginning of Service Api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HostListener } from '@angular/core';
 import { NgStyle } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NgStyle,RouterLink],
+  imports: [RouterOutlet,NgStyle,RouterLink,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { MovieApiServiceService } from './service/movie-api-service.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),MovieApiServiceService]
 };
