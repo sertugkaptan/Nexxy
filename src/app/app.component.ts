c22c3fba73adbb5eae9e592a9b8e56c7bbc75681
sertugkaptan <sertuhan00@gmail.com>
2024-02-04 23:25:18 +0100

    Movie Service api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { HostListener } from '@angular/core';
 import { NgStyle } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { Component, HostListener } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NgStyle,RouterLink,HttpClientModule],
+  imports: [RouterOutlet,NgStyle,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieApiServiceService } from '../../service/movie-api-service.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  constructor(private service:MovieApiServiceService){
+
+  }
+  ngOnInit(): void {
+    this.bannerData();
+  }
+
+  bannerData(){
+    this.service.bannerApiData().subscribe((result)=>{
+      console.log(result, 'bannerresult#');
+    })
+  }
 
 }
