064f8e3dfefeb7155df048dcb7ad0e330e34155c
sertugkaptan <sertuhan00@gmail.com>
2024-02-04 01:15:08 +0100

    Routing has been Established

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HostListener } from '@angular/core';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgStyle,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Nexxy';
+  navbg:any;
+
+  @HostListener('document:scroll') 
+  scrollOver(){
+    console.log(document.body.scrollTop,"scrolllength#");
+
+    if(document.body.scrollTop > 0 || document.documentElement.scrollTop>0){
+      this.navbg = {
+        'background-color':'#000000'
+      }
+    }else{
+      this.navbg={}
+    }
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
+import { SearchComponent } from './pages/search/search.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+    {path:'search',component:SearchComponent},
+    {path:'movie/:id',component:MovieDetailsComponent}
+];
+
