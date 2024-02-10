59006a241a4374a0c27d1d8fe53baecfd20e7c1e
sertugkaptan <sertuhan00@gmail.com>
2024-02-10 23:58:43 +0100

    Now Trending Movies in the search seaction

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -2,29 +2,37 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MovieApiServiceService } from '../../service/movie-api-service.service';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [ReactiveFormsModule,CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, RouterLink],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent implements OnInit {
-  constructor(private service: MovieApiServiceService) { }
+  searchResult: any;
+  trendingMovies: any = [];
 
+  constructor(private service: MovieApiServiceService) { }
+  
   ngOnInit(): void {
+    this.getTrendingMovies();
   }
 
-  searchResult: any;
   searchForm = new FormGroup({
     'movieName': new FormControl(null)
   });
-  
+
   submitForm() {
-    console.log(this.searchForm.value, 'searchform#');
     this.service.searchMovieWithName(this.searchForm.value).subscribe((result) => {
-      this.searchResult=result.results;
+      this.searchResult = result.results;
+    })
+  }
+  getTrendingMovies() {
+    this.service.trendingMovieApiData().subscribe((result) => {
+      this.trendingMovies = result.results
     })
   }
 }
